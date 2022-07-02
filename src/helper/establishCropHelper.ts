2804f322dc1ee1027690016b359436411efb861c
Arthur-Noh <dkagod97@gmail.com>
2022-07-02 22:38:17 +0900

    feat: crophelper

---
+++ src/helper/establishCropHelper.ts
@@ -0,0 +1,28 @@
+export const CorpType = [
+    { text: '주식회사', value: '주식회사' },
+    { text: '유한회사', value: '유한회사' },
+    { text: '유한책임회사', value: '유한책임회사' },
+    { text: '농업회사법인 주식회사', value: '농업회사법인 주식회사' },
+    { text: '어업회사법인 주식회사', value: '어업회사법인 주식회사' },
+];
+
+
+export const City = [
+    { text: '서울특별시', value: '서울특별시' },
+    { text: '경기도', value: '경기도' },
+    { text: '강원도', value: '강원도' },
+    { text: '경상남도', value: '경상남도' },
+    { text: '경상북도', value: '경상북도' },
+    { text: '광주광역시', value: '광주광역시' },
+    { text: '대구광역시', value: '대구광역시' },
+    { text: '대전광역시', value: '대전광역시' },
+    { text: '부산광역시', value: '부산광역시' },
+    { text: '세종특별자치시', value: '세종특별자치시' },
+    { text: '울산광역시', value: '울산광역시' },
+    { text: '인천광역시', value: '인천광역시' },
+    { text: '전라남도', value: '전라남도' },
+    { text: '전라북도', value: '전라북도' },
+    { text: '제주특별자치도', value: '제주특별자치도' },
+    { text: '충청남도', value: '충청남도' },
+    { text: '충청북도', value: '충청북도' },
+];
